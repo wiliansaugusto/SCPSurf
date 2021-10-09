0f3205f5fd29c608fc6b0a4944d84d001be2c09e
wiliansaugusto <wiliansaugusto@gmail.com>
2021-10-09 09:01:23 -0300

    Estrutura do Projeto

---
+++ src/test/java/br/com/achei/scpsurf/ScpSurfApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.achei.scpsurf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScpSurfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
