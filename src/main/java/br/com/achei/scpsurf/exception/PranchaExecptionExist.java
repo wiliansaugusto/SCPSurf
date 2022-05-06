f4e135e7e3c1a12511109b63004f602421ef1283
Willians Augusto <wiliansaugusto@gmail.com>
2022-05-06 10:25:25 -0300

    Restruturando o Projeto

--- src/main/java/br/com/achei/scpsurf/exception/PranchaExecptionExist.java
+++
@@ -1,8 +0,0 @@
-package br.com.achei.scpsurf.exception;
-
-public class PranchaExecptionExist extends Exception{
-    public PranchaExecptionExist(String prancha){
-        super(String.format("Prancha %s não existe no sistema.", prancha));
-
-    }
-}
